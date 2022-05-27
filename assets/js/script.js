c1470073801c2d9e9c52b3bfdc6b0f0138a6b1a8
prakashoc507 <prakashoc507@gmail.com>
2022-05-27 21:28:54 +0000

    add code for logic of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,26 @@ console.log("Welcome to TicTacToe")
 let turn = "X"
 let gameOver = false;
 const changeTurn = () => {
-    return turn === "X" ? "0" : "x"
+    return turn === "X" ? "0" : "X"
+}
+
+//Main game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let text = element.querySelector('.text');
+    element.addEventListener('click', () => {
+        if (text.innerText === '') {
+            text.innerText = turn;
+            turn = changeTurn();
+            checkWin();
+            document.getElementsByClassName("turn1")[0].innerText = "Turn for" + turn
+
+        }
+    })
+})
+
+
+// function for checkWin
+const checkWin = () => {
+
 }
